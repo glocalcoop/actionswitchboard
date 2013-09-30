a2bffe1b21e519774a1d668300e24a69d8512e52
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-09-30 16:28:10 -0400

    applies hide show behavior to find nav item,and css3 hideshow for search filters.

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -1,19 +1,23 @@
 (function($) {
   
   asb = {};
-
-  console.log( 'hi' );
   
   Drupal.behaviors.asb = {
+
     attach: function(context, settings) {
+
 //      console.log( 'Drupal.behaviors.asb.attach', context, settings );
-//      $( "#region-search" ).height( 0 );
-      console.log( $( "#navigation .nav menu-mlid-1205" ) );
-      $( "#navigation .nav menu-mlid-1205" ).click( function( e ) {
+      $( ".region-search" ).css('overflow','hidden');
+
+      // only hide search for people with JS.
+
+      $( "#navigation .menu-mlid-1205" ).click( function( e ) {
         e.preventDefault();
-        console.log( "!!!!", e );
+        $( ".region-search" ).toggleClass('active');
       });
+
     }
+
   }
 
 })(jQuery);
