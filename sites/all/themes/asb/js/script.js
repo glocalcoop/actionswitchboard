1485f7472d619e6b07f3ce76855e68240a854753
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-09-30 14:14:11 -0400

    tweaking js for hide show of search fields

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -1,14 +1,19 @@
-/**
- * @file
- * A JavaScript file for the asb theme.
- *
- */
+(function($) {
+  
+  asb = {};
 
-// for local config
-asb = {};
+  console.log( 'hi' );
+  
+  Drupal.behaviors.asb = {
+    attach: function(context, settings) {
+//      console.log( 'Drupal.behaviors.asb.attach', context, settings );
+//      $( "#region-search" ).height( 0 );
+      console.log( $( "#navigation .nav menu-mlid-1205" ) );
+      $( "#navigation .nav menu-mlid-1205" ).click( function( e ) {
+        e.preventDefault();
+        console.log( "!!!!", e );
+      });
+    }
+  }
 
-Drupal.behaviors.asb = {
-
-//  console.log( 'script.js loaded' );
-
-}
+})(jQuery);
