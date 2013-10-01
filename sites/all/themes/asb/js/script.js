97848233f346f0f41750b7b2e5f81d69ac124357
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-01 16:48:35 -0400

    more tweaks to hide/show behavior on search filters.

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -5,19 +5,16 @@
   Drupal.behaviors.asb = {
 
     attach: function(context, settings) {
-
 //      console.log( 'Drupal.behaviors.asb.attach', context, settings );
       $( ".region-search" ).css('overflow','hidden');
-//      $( ".region-search" ).css('height', 0);
-
-  $( ".region-search" ).addClass('atrest');
- $( ".region-search" ).addClass('hidden');
+      $( ".region-search" ).addClass('atrest');
+      $( ".region-search" ).addClass('hidden');
 
       // only hide search for people with JS.
 
       $( "#nav-find" ).click( function( e ) {
-        $( ".region-search" ).removeClass('atrest');
         e.preventDefault();
+        $( ".region-search" ).removeClass('atrest');
         $( ".region-search" ).toggleClass('hidden');
       });
 
