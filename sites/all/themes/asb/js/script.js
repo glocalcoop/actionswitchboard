72aec11e9a98c4810b5f33065f837a88c361f17a
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-01 16:36:04 -0400

    fufurther tweaks to sliding behavior

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -8,13 +8,16 @@
 
 //      console.log( 'Drupal.behaviors.asb.attach', context, settings );
       $( ".region-search" ).css('overflow','hidden');
-      $( ".region-search" ).addClass('hidden');
+//      $( ".region-search" ).css('height', 0);
+
+  $( ".region-search" ).addClass('atrest');
+ $( ".region-search" ).addClass('hidden');
 
       // only hide search for people with JS.
 
       $( "#nav-find" ).click( function( e ) {
+        $( ".region-search" ).removeClass('atrest');
         e.preventDefault();
-        $( ".region-search" ).toggleClass('active');
         $( ".region-search" ).toggleClass('hidden');
       });
 
