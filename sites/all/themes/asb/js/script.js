fd1443f2a5b6253425a44d1c48add097eec3d2c9
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-01 16:20:46 -0400

    tweaks to search filter positioning and behavior

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -8,12 +8,14 @@
 
 //      console.log( 'Drupal.behaviors.asb.attach', context, settings );
       $( ".region-search" ).css('overflow','hidden');
+      $( ".region-search" ).addClass('hidden');
 
       // only hide search for people with JS.
 
       $( "#nav-find" ).click( function( e ) {
         e.preventDefault();
         $( ".region-search" ).toggleClass('active');
+        $( ".region-search" ).toggleClass('hidden');
       });
 
     }
