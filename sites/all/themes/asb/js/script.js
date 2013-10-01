463fe2091ee21d238cb29801e3561ff069156a37
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-01 16:54:17 -0400

    search filters now show on load in /schemes

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -5,18 +5,23 @@
   Drupal.behaviors.asb = {
 
     attach: function(context, settings) {
-//      console.log( 'Drupal.behaviors.asb.attach', context, settings );
-      $( ".region-search" ).css('overflow','hidden');
-      $( ".region-search" ).addClass('atrest');
-      $( ".region-search" ).addClass('hidden');
 
-      // only hide search for people with JS.
+      if( context.body.className.substr( "page-schemes" ) == -1 ) {
 
-      $( "#nav-find" ).click( function( e ) {
-        e.preventDefault();
-        $( ".region-search" ).removeClass('atrest');
-        $( ".region-search" ).toggleClass('hidden');
-      });
+        console.log( 'Drupal.behaviors.asb.attach', context, settings );
+        $( ".region-search" ).css('overflow','hidden');
+        $( ".region-search" ).addClass('atrest');
+        $( ".region-search" ).addClass('hidden');
+
+        // only hide search for people with JS.
+
+        $( "#nav-find" ).click( function( e ) {
+          e.preventDefault();
+          $( ".region-search" ).removeClass('atrest');
+          $( ".region-search" ).toggleClass('hidden');
+        });
+
+      }
 
     }
 
