b56c170155410b385ad398a49142504597c4e265
Ross Glover <ross@ross.mayfirst.org>
2013-10-11 16:53:58 -0400

    Modified script.js to handle errors with Update tabs and modal field edits.

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -5,26 +5,28 @@
   Drupal.behaviors.asb = {
 
     attach: function(context, settings) {
-      console.log( context.body.className.indexOf( "page-schemes" ) );
-      if( context.body.className.indexOf( "page-schemes" ) < 0) {
+      // Catch TypeError in case form is called via ajax.
+      // Fixes problems with Update tab and field modal edit.
+      try {
+        console.log( context.body.className.indexOf( "page-schemes" ) );
+        if( context.body.className.indexOf( "page-schemes" ) < 0) {
+          // console.log( 'Drupal.behaviors.asb.attach', context, settings );
+          $( ".region-search" ).css('overflow','hidden');
+          $( ".region-search" ).addClass('atrest');
+          $( ".region-search" ).addClass('hidden');
 
-        console.log( 'Drupal.behaviors.asb.attach', context, settings );
-        $( ".region-search" ).css('overflow','hidden');
-        $( ".region-search" ).addClass('atrest');
-        $( ".region-search" ).addClass('hidden');
-
-        // only hide search for people with JS.
-
-        $( "#nav-find" ).click( function( e ) {
-          e.preventDefault();
-          $( ".region-search" ).removeClass('atrest');
-          $( ".region-search" ).toggleClass('hidden');
-        });
-
+          // only hide search for people with JS.
+          $( "#nav-find" ).click( function( e ) {
+            e.preventDefault();
+            $( ".region-search" ).removeClass('atrest');
+            $( ".region-search" ).toggleClass('hidden');
+          });
+        }
+      } catch (err) {
+	if (err.name === 'TypeError') {
+          console.log('Skipping javascript hide search.');
+        }
       }
-
     }
-
   }
-
 })(jQuery);
