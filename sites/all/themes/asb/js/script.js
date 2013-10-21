cd1c9a3f78b2c990e12dd11b2d98d02850cb63f6
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-21 14:01:01 -0400

    Fixing up the scheme search hide/show

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -1,4 +1,4 @@
-(function($) {
+( function($) {
   
   asb = {};
   
@@ -8,25 +8,25 @@
       // Catch TypeError in case form is called via ajax.
       // Fixes problems with Update tab and field modal edit.
       try {
-        console.log( context.body.className.indexOf( "page-schemes" ) );
+        //console.log( context.body.className.indexOf( "page-schemes" ) );
         if( context.body.className.indexOf( "page-schemes" ) < 0) {
           // console.log( 'Drupal.behaviors.asb.attach', context, settings );
-          $( ".region-search" ).css('overflow','hidden');
+          // $( ".region-search" ).css('overflow','hidden');
+//          $( ".region-search" ).css( 'height', $(".region-search").height() );
           $( ".region-search" ).addClass('atrest');
           $( ".region-search" ).addClass('hidden');
+        }
 
-          // only hide search for people with JS.
-          $( "#nav-find" ).click( function( e ) {
-            e.preventDefault();
-            $( ".region-search" ).removeClass('atrest');
-            $( ".region-search" ).toggleClass('hidden');
-          });
-        }
+        // only hide search for people with JS.
+        $( "#nav-find" ).click( function( e ) {
+          e.preventDefault();
+//          $( ".region-search" ).removeClass('atrest');
+          $( ".region-search" ).toggleClass('hidden');
+        });
       } catch (err) {
-	if (err.name === 'TypeError') {
-          console.log('Skipping javascript hide search.');
-        }
+      	if (err.name === 'TypeError') { console.log('Skipping javascript hide search.'); }
       }
     }
   }
+
 })(jQuery);
