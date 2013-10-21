cc52b3f416308476e93af15cfb5bcd946951cec7
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-21 17:13:25 -0400

    final tweaks to find drawer

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -11,21 +11,20 @@
         //console.log( context.body.className.indexOf( "page-schemes" ) );
         if( context.body.className.indexOf( "page-schemes" ) < 0) {
           // console.log( 'Drupal.behaviors.asb.attach', context, settings );
-          // $( ".region-search" ).css('overflow','hidden');
-//          $( ".region-search" ).css( 'height', $(".region-search").height() );
           $( ".region-search" ).addClass('atrest');
           $( ".region-search" ).addClass('hidden');
+        }else {
+          // only hide search for people with JS.
+          $( "#nav-find" ).click( function( e ) { 
+            e.preventDefault();
+            $( ".region-search" ).removeClass('atrest');
+            $( ".region-search" ).toggleClass('hidden');
+          });          
         }
-
-        // only hide search for people with JS.
-        $( "#nav-find" ).click( function( e ) {
-          e.preventDefault();
-//          $( ".region-search" ).removeClass('atrest');
-          $( ".region-search" ).toggleClass('hidden');
-        });
       } catch (err) {
       	if (err.name === 'TypeError') { console.log('Skipping javascript hide search.'); }
       }
+
     }
   }
 
