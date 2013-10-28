98834c40fbd6fe0ae1e061bc2eff0c292a942a5e
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-10-28 13:42:24 -0700

    fixed drawer slidey for search.
    if( context.body.className.indexOf("page-schemes") was supposed to be just an if, not if else (instantiation of click wasnt happening)

--- sites/all/themes/asb/js/script.js
+++ sites/all/themes/asb/js/script.js
@@ -8,19 +8,20 @@
       // Catch TypeError in case form is called via ajax.
       // Fixes problems with Update tab and field modal edit.
       try {
-        //console.log( context.body.className.indexOf( "page-schemes" ) );
+        console.log( context, context.body.className.indexOf( "page-schemes" ) );
         if( context.body.className.indexOf( "page-schemes" ) < 0) {
           // console.log( 'Drupal.behaviors.asb.attach', context, settings );
           $( ".region-search" ).addClass('atrest');
           $( ".region-search" ).addClass('hidden');
-        }else {
-          // only hide search for people with JS.
-          $( "#nav-find" ).click( function( e ) { 
-            e.preventDefault();
-            $( ".region-search" ).removeClass('atrest');
-            $( ".region-search" ).toggleClass('hidden');
-          });          
         }
+        console.log("@@@@@@@@@");
+        // only hide search for people with JS.
+        $( "#nav-find" ).click( function( e ) { 
+          e.preventDefault();
+          console.log("@@@@@@");
+          $( ".region-search" ).removeClass('atrest');
+          $( ".region-search" ).toggleClass('hidden');
+        });
       } catch (err) {
       	if (err.name === 'TypeError') { console.log('Skipping javascript hide search.'); }
       }
