aea70eada25bfac409f215f6c1bafbbf56c76c29
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-11-15 13:09:27 -0800

    adding autopager manually in template.php “because of unknown error”

--- sites/all/themes/asb/template.php
+++ sites/all/themes/asb/template.php
@@ -486,9 +486,9 @@ function asb_page_alter( &$variables ) {
 }
 
 function asb_views_post_render(&$view) {
-  dpm( (module_exists('libraries') ) && file_exists(libraries_get_path('autopager') ) );
   if($view->current_display == 'block_1' || $view->current_display == "page_1" ){
     $scripts = drupal_add_js();
+    drupal_add_js('sites/all/libraries/autopager/jquery.autopager-1.0.0.js');
     unset($scripts['module']['sites/all/modules/contrib/views_infinite_scroll/js/views_infinite_infinitescroll.js']);
     drupal_add_js('sites/all/themes/asb/js/alter_infinitescroll.js');
   }
