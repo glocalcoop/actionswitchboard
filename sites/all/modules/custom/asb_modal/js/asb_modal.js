09fed1ce04527e86823ed569fae9b9f758a7cce6
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-12-16 16:19:01 -0500

    tweaks to modal module, and overriding modal markup.

--- sites/all/modules/custom/asb_modal/js/asb_modal.js
+++ sites/all/modules/custom/asb_modal/js/asb_modal.js
@@ -3,16 +3,18 @@
 */
 (function ($) {
 Drupal.theme.prototype.asb_modal = function () {
-    // alert(Drupal.settings.asb_modal.types);
+    alert(Drupal.settings.asb_modal.types);
     var html = '';
     html += '<div id="ctools-modal" class="popups-box">';
-    html += '  <div class="ctools-modal-content ctools-modal-asb-modal-update">';
-    html += '    <div class="modal-head-wrapper">';
-    html += '    <span class="modal-update-title" style="display:none;">Add Update</span>';
-    html += '    <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
-    html += '    </div>';
-    html += '    <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
-    html += '  </div>';
+    html += '   <div class="ctools-modal-content ctools-modal-asb-modal-update">';
+    html += '       <div class="modal-content-wrapper">'
+    html += '           <div class="modal-header modal-head-wrapper">';
+    html += '               <span class="modal-update-title" style="display:none;">Add Update</span>';
+    html += '               <span class="popups-close"><a class="close ctools-close-modal" href="#"><span>Close Window</span></a></span>';
+    html += '           </div>';
+    html += '           <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+    html += '       </div>';
+    html += '   </div>';
     html += '</div>';
     return html;
 }
--- sites/all/themes/asb/js/scripts.js
+++ sites/all/themes/asb/js/scripts.js
@@ -1,6 +1,26 @@
 ( function($) {
   
   asb = {};
+  
+    /**
+   * Provide the HTML to create the modal dialog.
+   */
+  Drupal.theme.prototype.CToolsModalDialog = function () {
+//    alert( "Overriding CToolModalDialog" );
+    var html = '';
+    html += '<div id="ctools-modal" class="popups-box">';
+    html += '   <div class="ctools-modal-content ctools-modal-asb-modal-update">';
+    html += '       <div class="modal-content-wrapper">'
+    html += '           <div class="modal-head-wrapper">';
+    html += '               <span class="modal-update-title" style="display:none;">Add Update</span>';
+    html += '               <span class="popups-close"><a class="close ctools-close-modal" href="#"><span>Close Window</span></a></span>';
+    html += '           </div>';
+    html += '           <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+    html += '       </div>';
+    html += '   </div>';
+    html += '</div>';
+    return html;
+  }
 
   Drupal.behaviors.asb = {
     attach: function(context, settings) {
