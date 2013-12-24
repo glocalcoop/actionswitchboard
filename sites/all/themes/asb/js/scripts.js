715a4be4b912550c4d3b7fc0fecd5fa456e4a557
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-12-24 12:36:15 -0200

    Restyling of scheme edit modals for add/edit image, add/edit video.

--- sites/all/themes/asb/js/scripts.js
+++ sites/all/themes/asb/js/scripts.js
@@ -2,19 +2,25 @@
   
   asb = {};
   
+  /*
+@Todo add class new to messages that have <mark class="new"></mark>
+  */
+
+  
     /**
    * Provide the HTML to create the modal dialog.
    */
-  Drupal.theme.prototype.CToolsModalDialog = function () {
+  Drupal.theme.prototype.CToolsModalDialog = function (a,b) {
 //    alert( "Overriding CToolModalDialog" );
+console.log( a,b );
     var html = '';
     html += '<div id="ctools-modal" class="popups-box">';
     html += '   <div class="ctools-modal-content ctools-modal-asb-modal-update">';
     html += '       <div class="modal-content-wrapper">'
-    html += '           <div class="modal-head-wrapper">';
+    html += '           <header class="modal-head-wrapper">';
     html += '               <span class="modal-update-title" style="display:none;">Add Update</span>';
     html += '               <span class="popups-close"><a class="close ctools-close-modal" href="#"><span>Close Window</span></a></span>';
-    html += '           </div>';
+    html += '           </header>';
     html += '           <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
     html += '       </div>';
     html += '   </div>';
