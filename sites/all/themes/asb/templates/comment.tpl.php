fd8fb35d9cd710d9ba891b6f9ba7fa4467d53987
Pea <pea@misfist.com>
2014-01-10 23:55:32 -0500

    Modified comment template to add a few additional css classes

---
+++ sites/all/themes/asb/templates/comment.tpl.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for comments.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728216
+ */
+?>
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="user-meta">
+    <?php print $picture; ?>
+  </div>
+  <header>
+    <p class="submitted">
+      <?php print $submitted; ?>
+      <?php print $permalink; ?>
+    </p>
+
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h3<?php print $title_attributes; ?>>
+        <?php print $title; ?>
+        <?php if ($new): ?>
+          <mark class="new"><span><?php print $new; ?></span></mark>
+        <?php endif; ?>
+      </h3>
+    <?php elseif ($new): ?>
+      <mark class="new"><span><?php print $new; ?></span></mark>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($status == 'comment-unpublished'): ?>
+      <mark class="unpublished"><span><?php print t('Unpublished'); ?></span></mark>
+    <?php endif; ?>
+  </header>
+
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <?php if ($signature): ?>
+    <footer class="user-signature clearfix">
+      <?php print $signature; ?>
+    </footer>
+  <?php endif; ?>
+
+  <?php print render($content['links']) ?>
+
+<?php
+// echo '<pre>';
+// var_dump($user);
+// echo '</pre>';
+?>
+
+</article>
