cb80bee1c2d28cf7eb90a0bbf064ba1d8e91047e
Pea <pea@misfist.com>
2014-01-13 22:39:21 -0500

    Commented out print  in the content area because title appears in content header area

--- sites/all/themes/asb/templates/node.tpl.php
+++ sites/all/themes/asb/templates/node.tpl.php
@@ -108,7 +108,7 @@ global $user;
         <?php print render($title_prefix); ?>
         
         <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <!-- <h1 class="title" id="page-title"><?php print $title; ?></h1> -->
         <?php endif; ?>
 
         <?php print render($title_suffix); ?>
