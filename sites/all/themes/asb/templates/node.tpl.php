b496e0105556098e22ba8549e71bc9f05e011c9c
Pea <pea@misfist.com>
2014-01-19 21:04:30 -0500

    Removed errant comment close tag

--- sites/all/themes/asb/templates/node.tpl.php
+++ sites/all/themes/asb/templates/node.tpl.php
@@ -107,9 +107,7 @@ global $user;
         
         <?php print render($title_prefix); ?>
         
-        <?php if ($title): ?>
-        <!-- <h1 class="title" id="page-title"><?php print $title; ?></h1> -->
-        <?php endif; ?>
+       
 
         <?php print render($title_suffix); ?>
 
