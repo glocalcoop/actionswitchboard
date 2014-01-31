9e905a0981dcc2ebf9852e9225f6e46e2d24f044
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-01-31 13:51:45 -0500

    fixed syntax error in asb.highlight_new_messages

--- sites/all/themes/asb/js/scripts.js
+++ sites/all/themes/asb/js/scripts.js
@@ -34,7 +34,7 @@
   asb.highlight_new_messages = function() {
     $( ".section-dashboard tr, .section-messages tr" ).each( function(){
       var tr = $(this);
-      if( tr.find('mark.new'). ) $(this).addClass('new');
+      if( tr.find('mark.new').length ) $(this).addClass('new');
     });
   }
 
