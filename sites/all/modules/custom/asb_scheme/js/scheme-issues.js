894a570a25c3710238054a8e112574f2f8a75bab
Ross Glover <ross@ross.mayfirst.org>
2014-02-08 12:24:24 -0600

    Narrowing jquery selector to avoid unintentional clicking of dom items.

--- sites/all/modules/custom/asb_scheme/js/scheme-issues.js
+++ sites/all/modules/custom/asb_scheme/js/scheme-issues.js
@@ -1,7 +1,7 @@
 (function($) {
   Drupal.behaviors.asbScheme = {
     attach: function(context, settings) {
-      jQuery('input[name=field_issues_goals[und]]:checked').click();
+      jQuery('#edit-field-issues-goals').find('input[name=field_issues_goals[und]]:checked').click();
       jQuery('#edit-field-scheme-goals-taxonomy').show();
     }
   }
