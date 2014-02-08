1dc53224a74d4c9a2f8c2834b632617d6d09864e
Pea <pea@misfist.com>
2014-02-07 21:05:37 -0500

    Added conditional heading above scheme comments.

--- sites/all/themes/asb/templates/comment-wrapper.tpl.php
+++ sites/all/themes/asb/templates/comment-wrapper.tpl.php
@@ -38,13 +38,21 @@
 ?>
 <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($content['comment_form']): ?>
-    <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
+      <?php if($node->type == 'scheme') { ?>
+      <h2 class="title comment-form"><?php print t('Add a Public Comment About this Scheme'); ?></h2>
+      <?php } else { ?>
+      <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
+      <?php } ?>
     <?php print render($content['comment_form']); ?>
   <?php endif; ?>
 
   <?php if ($content['comments'] && $node->type != 'forum'): ?>
     <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Comments'); ?></h2>
+      <?php if($node->type == 'scheme') { ?>
+      <h2 class="title scheme-comments"><?php print t('Public Comments About this Scheme'); ?></h2>
+      <?php } else { ?>
+      <h2 class="title"><?php print t('Comments'); ?></h2>
+      <?php } ?>
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
