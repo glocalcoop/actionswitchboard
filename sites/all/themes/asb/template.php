752b111947c84652b20021cd87086b8e41f797f9
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-03-30 08:55:43 -0400

    scheme, user header alignment

--- sites/all/themes/asb/template.php
+++ sites/all/themes/asb/template.php
@@ -366,6 +366,7 @@ function asb_preprocess_node(&$variables, $hook) {
       }
     }
     if (node_access("update", $variables['node'], $account) === TRUE) {
+      $variables['classes_array'][] = 'edit';
       $variables['issue_edit'] = '<a class="ctools-use-modal ctools-modal-mfe-modal" href="/mfe-single-modal-callback/nojs/' .arg(1) .'/scheme/field_issues_goals">Edit</a>';
       // This javascript is added in asb_scheme module, but was not
       // recognized by chromium on modal edit.
